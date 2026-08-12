b2c64b79803616188b26409c87edd3138efedb4b
Gary Scudder <gary_scudder@yahoo.com>
2026-08-12 05:26:00 -0400

    Remove EPG Manager files from Home Network Monitor

--- mount_shares.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sleep 15
-osascript -e 'mount volume "smb://GarysNas/EPG"'
-osascript -e 'mount volume "smb://GarysNas/Fire TV"'
-osascript -e 'mount volume "smb://GarysNas/Public"'
-osascript -e 'mount volume "smb://GarysNas/Plex"'
--- restart_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Stopping server on port 5001..."
-kill $(lsof -ti:5001) 2>/dev/null
-sleep 1
-echo "Starting server..."
-cd ~/epg
-python3.11 server.py
